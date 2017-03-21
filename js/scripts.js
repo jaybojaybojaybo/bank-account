0ab99432ea420539b61effc08bb326ff31eea04d
Joshua Fairchild and Jasun Feddema <jasun@>
2017-03-20 20:37:24 -0700

    build HTML front-end site and create js Account constructor and 2 empty prototype methods for the Account object.

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function Account(name, balance) {
+  this.accountName = name;
+  this.accountBalance = balance;
+}
+
+Account.prototype.deposit = function(balance) {
+
+}
+
+Account.prototype.withdraw = function(balance) {
+
+}
+
+$(function() {
+  
+});
